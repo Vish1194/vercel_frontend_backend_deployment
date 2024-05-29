6b2cb25d6c7351fa0f1db4bb7c48f78cec5cfb54
Vish1194 <152699085+Vish1194@users.noreply.github.com>
2024-05-29 14:58:37 +0530

    added more api

--- backend/index.js
+++ backend/index.js
@@ -53,146 +53,146 @@ app.post('/login',async (req,res)=>{
 })
 
 
-// app.post('/registerUser', async (req,res)=>{
-//     const {fname,lname,email,mobile,password} = req.body;
-//     try {
-//         const [row] = await (await con).query('Insert into user(fname,lname,email,mobile,password) values(?,?,?,?,?)',[fname,lname,email,mobile,password]);
-//         if(row.affectedRows>0){
-//             res.status(200).send('Register Successful.');
-//         }
-//     } catch (error) {
-//         if(error){
-//             res.status(500).send('Register Unsuccessful.');
-//         }
-//     }
-// })
+app.post('/registerUser', async (req,res)=>{
+    const {fname,lname,email,mobile,password} = req.body;
+    try {
+        const [row] = await (await con).query('Insert into user(fname,lname,email,mobile,password) values(?,?,?,?,?)',[fname,lname,email,mobile,password]);
+        if(row.affectedRows>0){
+            res.status(200).send('Register Successful.');
+        }
+    } catch (error) {
+        if(error){
+            res.status(500).send('Register Unsuccessful.');
+        }
+    }
+})
 
 
-// app.post('/sell-property',async (req,res)=>{
-//     const {place,address,bedrooms,bathrooms,amenities,price,user_id} = req.body;
-//     // console.log(req.body);
-//     try {
-//         const [row] = await (await con).query('Insert into properties (place,address,bedrooms,bathrooms,amenities,price,user_id) values (?,?,?,?,?,?,?)',[place,address,bedrooms,bathrooms,amenities,price,user_id]);
-//         if(row.affectedRows>0){
-//             res.status(200).send('Property registered Successful.');
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         if(error){
-//             res.status(500).send('Property register Unsuccessful.');
-//         }
-//     }
-// })
+app.post('/sell-property',async (req,res)=>{
+    const {place,address,bedrooms,bathrooms,amenities,price,user_id} = req.body;
+    // console.log(req.body);
+    try {
+        const [row] = await (await con).query('Insert into properties (place,address,bedrooms,bathrooms,amenities,price,user_id) values (?,?,?,?,?,?,?)',[place,address,bedrooms,bathrooms,amenities,price,user_id]);
+        if(row.affectedRows>0){
+            res.status(200).send('Property registered Successful.');
+        }
+    } catch (error) {
+        console.log(error);
+        if(error){
+            res.status(500).send('Property register Unsuccessful.');
+        }
+    }
+})
 
-// app.post('/all-properties',async (req,res)=>{
-//     const {id} = req.body;
-//     try {
-//         const [row] = await (await con).query('select * from properties where user_id != ?',[id]);
-//         if(row.length>0){
-//             res.status(200).send(row);
-//         }
-//     } catch (error) {
-//         if(error){
-//             res.status(500).send('Internal Server Error.')
-//         }
-//     }
+app.post('/all-properties',async (req,res)=>{
+    const {id} = req.body;
+    try {
+        const [row] = await (await con).query('select * from properties where user_id != ?',[id]);
+        if(row.length>0){
+            res.status(200).send(row);
+        }
+    } catch (error) {
+        if(error){
+            res.status(500).send('Internal Server Error.')
+        }
+    }
 
-// })
+})
 
-// app.post('/get-property',async (req,res)=>{
-//     const {id} = req.body;
-//     try {
-//         const [row] = await (await con).query('select place,address,bedrooms,bathrooms,amenities,price,fname,lname,email,mobile,user_id,id from properties,user where user_id=id and property_id=?',[id]);
-//         if(row.length>0){
-//             res.status(200).send(row[0]);
-//         }
-//     } catch (error) {
-//         if(error){
-//             res.status(500).send('Internal Server Error.')
-//         }
-//     }
+app.post('/get-property',async (req,res)=>{
+    const {id} = req.body;
+    try {
+        const [row] = await (await con).query('select place,address,bedrooms,bathrooms,amenities,price,fname,lname,email,mobile,user_id,id from properties,user where user_id=id and property_id=?',[id]);
+        if(row.length>0){
+            res.status(200).send(row[0]);
+        }
+    } catch (error) {
+        if(error){
+            res.status(500).send('Internal Server Error.')
+        }
+    }
     
-// })
+})
 
-// app.post('/my-property',async (req,res)=>{
-//     const {id} = req.body;
-//     try {
-//         const [row] = await (await con).query('select * from properties where user_id=?',[id]);
-//         if(row.length>0){
-//             res.status(200).send(row);
-//         }
-//     } catch (error) {
-//         if(error){
-//             res.status(500).send('Internal Server Error.')
-//         }
-//     }
+app.post('/my-property',async (req,res)=>{
+    const {id} = req.body;
+    try {
+        const [row] = await (await con).query('select * from properties where user_id=?',[id]);
+        if(row.length>0){
+            res.status(200).send(row);
+        }
+    } catch (error) {
+        if(error){
+            res.status(500).send('Internal Server Error.')
+        }
+    }
     
-// })
+})
 
-// app.post('/remove-property',async (req,res)=>{
-//     const {id} = req.body;
-//     try {
-//         const [row] = await (await con).query('DELETE FROM properties WHERE property_id=?',[id]);
-//         if(row.affectedRows>0){
-//             res.status(200).send('Removed Succesfully');
-//         }
-//     } catch (error) {
-//         if(error){
-//             console.log(error)
-//             res.status(500).send('Internal Server Error.')
-//         }
-//     }
+app.post('/remove-property',async (req,res)=>{
+    const {id} = req.body;
+    try {
+        const [row] = await (await con).query('DELETE FROM properties WHERE property_id=?',[id]);
+        if(row.affectedRows>0){
+            res.status(200).send('Removed Succesfully');
+        }
+    } catch (error) {
+        if(error){
+            console.log(error)
+            res.status(500).send('Internal Server Error.')
+        }
+    }
     
-// })
+})
 
 
-// app.post('/update-property',async (req,res)=>{
-//     const {place,address,bedrooms,bathrooms,amenities,price,id} = req.body;
-//     console.log(req.body);
-//     try {
-//         const [row] = await (await con).query('update properties set place=?,address=?,bedrooms=?,bathrooms=?,amenities=?,price=? where property_id=?',[place,address,bedrooms,bathrooms,amenities,price,id]);
-//         if(row.affectedRows>0){
-//             res.status(200).send('Property Updated Successful.');
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         if(error){
-//             console.log(error)
-//             res.status(500).send('Property Update Unsuccessful.');
-//         }
-//     }
-// })
+app.post('/update-property',async (req,res)=>{
+    const {place,address,bedrooms,bathrooms,amenities,price,id} = req.body;
+    console.log(req.body);
+    try {
+        const [row] = await (await con).query('update properties set place=?,address=?,bedrooms=?,bathrooms=?,amenities=?,price=? where property_id=?',[place,address,bedrooms,bathrooms,amenities,price,id]);
+        if(row.affectedRows>0){
+            res.status(200).send('Property Updated Successful.');
+        }
+    } catch (error) {
+        console.log(error);
+        if(error){
+            console.log(error)
+            res.status(500).send('Property Update Unsuccessful.');
+        }
+    }
+})
 
-// app.post('/search', async (req,res)=>{
-//     const {search} = req.body;
-//     console.log(isNaN(search));
-//     if (!isNaN(search)) {
-//         let num = parseInt(search);
-//         try {
-//             const [row] = await (await con).query('select * from properties where price>=? order by price',[num]);
-//             if(row.length>0){
-//                 res.status(200).send(row);
-//             }
-//         } catch (error) {
-//             if(error){
-//                 res.status(500).send('Internal Server Error.')
-//             }
-//         }
+app.post('/search', async (req,res)=>{
+    const {search} = req.body;
+    console.log(isNaN(search));
+    if (!isNaN(search)) {
+        let num = parseInt(search);
+        try {
+            const [row] = await (await con).query('select * from properties where price>=? order by price',[num]);
+            if(row.length>0){
+                res.status(200).send(row);
+            }
+        } catch (error) {
+            if(error){
+                res.status(500).send('Internal Server Error.')
+            }
+        }
 
-//     } 
-//     else if (isNaN(search)) {
-//         try {
-//             const [row] = await (await con).query('select * from properties where place=?',[search]);
-//             if(row.length>0){
-//                 res.status(200).send(row);
-//             }
-//         } catch (error) {
-//             if(error){
-//                 res.status(500).send('Internal Server Error.')
-//             }
-//         }
-//     } 
-//     else {
-//       res.status(500).send('Invalid Search')
-//     }
-// })
+    } 
+    else if (isNaN(search)) {
+        try {
+            const [row] = await (await con).query('select * from properties where place=?',[search]);
+            if(row.length>0){
+                res.status(200).send(row);
+            }
+        } catch (error) {
+            if(error){
+                res.status(500).send('Internal Server Error.')
+            }
+        }
+    } 
+    else {
+      res.status(500).send('Invalid Search')
+    }
+})
