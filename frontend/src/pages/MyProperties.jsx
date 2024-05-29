d08a668405d2826ae8309d7d647b665c03934335
Vish1194 <152699085+Vish1194@users.noreply.github.com>
2024-05-29 19:06:56 +0530

    style update

--- frontend/src/pages/MyProperties.jsx
+++ frontend/src/pages/MyProperties.jsx
@@ -23,8 +23,8 @@ const MyProperties = () =>{
         console.log(data);
     },[])
     return(
-        <>
-        <h2 className="text-center my-4" style={{minHeight:"90vh"}}>My Posted Properties</h2>
+        <div style={{minHeight:"90vh"}}>
+        <h2 className="text-center my-4">My Posted Properties</h2>
             <div className="property-listing">
                 {property.map((j) => (
                     <PropertyCard key={j.property_id} isDashboard={false} id={j.property_id} place={j.place} address={j.address} bedrooms={j.bedrooms} bathroooms={j.bathroooms} amenities={j.amenities} price={j.price}/>
@@ -33,7 +33,7 @@ const MyProperties = () =>{
         <div className="text-center py-5">
             <Link to='/dashboard' className="btn btn-warning">Go Back</Link>
         </div>
-        </>
+        </div>
     )
 }
 export default MyProperties;
